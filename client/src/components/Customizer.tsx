5e86a6a6702d85ac2dc7e8f6376ba530ee3317a7
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-22 21:47:04 +0500

    -

--- client/src/components/Customizer.tsx
+++ client/src/components/Customizer.tsx
@@ -22,13 +22,59 @@ import { EditorTabs, FilterTabs, DecalTypes } from '@/app/config/constants'
 // Animations
 import { fadeAnimation, slideAnimation } from '@/app/config/motion'
 
+// Components
+import { AIPicker, FilePicker, ColorPicker, Tab } from '.'
+import { Button } from './ui/button'
 
 type Props = {}
 
 const Customizer = (props: Props) => {
-  return (
-    <div>Customizer</div>
-  )
+	// Using Global State Store
+	const snap = useSnapshot(state)
+
+	return (
+		<AnimatePresence>
+			{!snap.intro && (
+				<>
+					<motion.div
+						key='custom'
+						className='absolute top-0 left-0 z-10'
+						{...slideAnimation('left')}
+					>
+						<div className='flex items-center min-h-screen'>
+							<div className='editortabs-container tabs'>
+								{EditorTabs.map((tab) => (
+									// @ts-ignore
+									<Tab key={tab.name} tab={tab} handleClick={() => {}} />
+								))}
+							</div>
+						</div>
+					</motion.div>
+					<motion.div
+						className='absolute z-10 top-5 right-5'
+						{...fadeAnimation}
+					>
+						<Button
+							onClick={() => (state.intro = true)}
+							className={`font-bold text-sm bg-[${snap?.color}] w-fit px-5 py-3 text-black hover:bg-[#dcae43]`}
+						>
+							Go Back
+						</Button>
+					</motion.div>
+
+					<motion.div
+						className='filtertabs-container'
+						{...slideAnimation('up')}
+					>
+						{FilterTabs.map((tab) => (
+							// @ts-ignore
+							<Tab key={tab.name} isFilterTab isActiveTab="" tab={tab} handleClick={() => {}} />
+						))}
+					</motion.div>
+				</>
+			)}
+		</AnimatePresence>
+	)
 }
 
 export default Customizer
---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+
+export { AIPicker, FilePicker, ColorPicker, Tab };
