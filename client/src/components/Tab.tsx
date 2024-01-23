b43df0a240a33a09619356e0b8a87eebe8519657
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2024-01-23 09:30:05 +0500

    -

--- client/src/components/Tab.tsx
+++ client/src/components/Tab.tsx
@@ -1,11 +1,48 @@
+'use client'
+
 import React from 'react'
+import { StaticImageData } from 'next/image'
 
-type Props = {}
+// styles
+import { cn } from '@/lib/utils'
 
-const Tab = (props: Props) => {
-  return (
-    <div>Tab</div>
-  )
+// State Management
+import { useSnapshot } from 'valtio'
+import state from '@/store'
+
+interface Props {
+	tab: {
+		name: string
+		icon: StaticImageData
+	}
+	isFilterTab?: boolean
+	isActiveTab?: string
+	handleClick: () => void
+}
+
+const Tab = ({
+	tab: { name, icon },
+	isFilterTab,
+	isActiveTab,
+	handleClick,
+}: Props) => {
+	// Global State Store
+	const snap = useSnapshot(state)
+
+	const activeStyle =
+		isFilterTab && isActiveTab
+			? { backgroundColor: snap.color, opacity: 0.5 }
+			: { backgroundColor: snap.color, opacity: 1 }
+
+	return (
+		<div
+			key={name}
+			className={cn('tab-btn',
+			isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4')}
+			onClick={handleClick}
+			style={activeStyle}
+		></div>
+	)
 }
 
 export default Tab
